679e1010105a89270b44d55f288689935eabee8b
mrhackerhacker696-svg <mrhackerhacker696@gmail.com>
2026-05-05 00:48:31 +0530

    jhgvh

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -69,7 +69,7 @@ const Sidebar = ({ isOpen, onClose }) => {
       )}
 
       <aside className={`
-        fixed top-0 left-0 bottom-0 z-50 w-64 bg-white border-r border-slate-100 transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:h-full lg:z-0
+        fixed top-0 left-0 bottom-0 z-50 w-64 bg-white border-r border-slate-100 transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:sticky lg:top-24 lg:h-[calc(100vh-6rem)] overflow-y-auto scrollbar-thin
         ${isOpen ? 'translate-x-0' : '-translate-x-full'}
       `}>
         <div className="flex flex-col h-full p-4 overflow-y-auto">
