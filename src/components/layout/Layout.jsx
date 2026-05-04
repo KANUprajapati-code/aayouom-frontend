f8c732558856f1389dce3ed69f25bffc444b8622
mrhackerhacker696-svg <mrhackerhacker696@gmail.com>
2026-05-05 00:51:18 +0530

    jhlkkl

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -13,23 +13,25 @@ const Layout = () => {
   const location = useLocation();
 
   return (
-    <div className="min-h-screen flex flex-col bg-surface-light">
+    <div className="h-screen flex flex-col bg-surface-light overflow-hidden">
       <Navbar onMenuClick={() => setIsSidebarOpen(true)} />
       
-      <div className="flex flex-1 max-w-[1600px] mx-auto w-full relative">
+      <div className="flex flex-1 max-w-[1600px] mx-auto w-full relative overflow-hidden">
         <Sidebar 
           isOpen={isSidebarOpen} 
           onClose={() => setIsSidebarOpen(false)} 
         />
         
-        <main className="flex-1 w-full px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-10 min-w-0 pb-24 md:pb-10">
-          <div className="max-w-7xl mx-auto w-full">
-            <Outlet />
-          </div>
-        </main>
+        <div className="flex-1 flex flex-col overflow-y-auto scrollbar-thin">
+          <main className="flex-grow w-full px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-10 min-w-0 pb-10">
+            <div className="max-w-7xl mx-auto w-full">
+              <Outlet />
+            </div>
+          </main>
+          <Footer />
+        </div>
       </div>
       
-      <Footer />
       <MobileNav />
     </div>
   );
